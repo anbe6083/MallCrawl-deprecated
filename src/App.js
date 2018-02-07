4366631281a67ab0d3cfe98d1673e0c378c2e55b
anbe6083 <anbe6083@colorado.edu>
2018-02-06 19:25:04 -0800

    State updates from yelp api

--- src/App.js
+++ src/App.js
@@ -7,12 +7,78 @@ import {Alert} from 'react-bootstrap';
 
 class App extends Component {
   state = {
-    response: ''
-  };
+    'id': '',
+    'name': 'test',
+    'image_url': '',
+    'is_closed': false,
+    'url': '',
+    'review_count': 0,
+    'categories': [
+    {
+    'alias': '',
+    'title': ''
+    }
+    ],
+    'rating': 0,
+    'coordinates': {
+    'latitude': 0,
+    'longitude': 0
+    },
+    'transactions': [],
+    'price': '',
+    'location': {
+    'address1': '',
+    'address2': '',
+    'address3': '',
+    'city': '',
+    'zip_code': '',
+    'country': '',
+    'state': '',
+    'display_address': [
+    ]
+    },
+    'phone': '',
+    'display_phone': '',
+    'distance': 0
+    };
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ 
+        id: res.id,
+        name: res.name,
+        image_url: res.image_url,
+        is_closed: res.is_closed,
+        url: res.url,
+        review_count: res.review_count,
+        categories: [
+        {
+        alias: res.alias,
+        title: res.title
+        }
+        ],
+        rating: res.rating,
+        coordinates: {
+        latitude: res.latitude,
+        longitude: res.longitude
+        },
+        transactions: res.transactions,
+        price: res.price,
+        location: {
+        address1: res.address1,
+        address2: res.address2,
+        address3: res.address3,
+        city: res.city,
+        zip_code: res.zip_code,
+        country: res.country,
+        state: res.state,
+        display_address: [
+        ]
+        },
+        phone: res.phone,
+        display_phone: res.display_phone,
+        distance: res.distance
+      }))
       .catch(err => console.log(err));
   }
 
@@ -26,17 +92,17 @@ class App extends Component {
   };
 
   render() {
-    console.log(JSON.stringify(this.state.response));
+    console.log(JSON.stringify(this.state.id));
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           {this.state.response}
         </p>
-        <Nightlife_Entry />
+        <Nightlife_Entry name={this.state.name} />
       </div>
     );
   }
--- src/nightlife_entry.js
+++ src/nightlife_entry.js
@@ -8,8 +8,8 @@ import { Button } from 'react-bootstrap';
 import thumbnail from './thumbnail.png';
 import './nightlife_entry_style.css';
 export default class Nightlife_Entry extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             yelpData: [],
         }
@@ -36,8 +36,9 @@ export default class Nightlife_Entry extends Component {
     }*/
 
     render() {
+        console.log(this.props.name);
         return (
-
+            
             <Col xs={6} md={3} className="nightlife_entry border border-dark rounded">
                 <style type="text/css">{`
             .nightlife_entry {
@@ -51,6 +52,9 @@ export default class Nightlife_Entry extends Component {
                 <Media.Heading align="top" className="entry_img">
                 </Media.Heading>
                 <div className="Entry_text">
+                    <h1>
+                        {this.props.name}            
+                    </h1>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt, leo sit amet malesuada volutpat, justo purus imperdiet quam, eget feugiat nisi nisi vel turpis. Suspendisse tempus lorem in nisi commodo condimentum. Donec facilisis feugiat est sed volutpat. Aenean fermentum tristique quam, eu efficitur velit bibendum non. Donec facilisis posuere tortor a lacinia. Morbi blandit ligula non feugiat pulvinar. Integer malesuada augue quis turpis porttitor pretium. Pellentesque aliquam ullamcorper arcu, vel vulputate felis fringilla sit amet. Nam mauris sapien, cursus pharetra porttitor non, faucibus nec ante. Nulla neque lorem, blandit at ligula sed, feugiat volutpat felis. Nulla pretium ipsum euismod mauris eleifend venenatis. Vestibulum vel vulputate ex, sit amet posuere mauris. Sed eleifend volutpat nisl id feugiat. Interdum et malesuada fames ac ante ipsum primis in faucibus.
                 </div>
                 <Button bsStyle='success'> Going </Button>
